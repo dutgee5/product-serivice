0297964a9d628f987317618dcb0a765008e40f5a
Coders <muzodone5@gmail.com>
2023-01-26 22:33:20 +0300

    swagger added

---
+++ src/main/java/com/eleiatech/stockmanagement/productserivice/ProductSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.eleiatech.stockmanagement.productserivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSeriviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleiatech/stockmanagement/productserivice/ProductSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleiatech.stockmanagement.productserivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSeriviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
